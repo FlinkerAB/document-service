c6dc9c3ef9a13e445f7b340fa2c3f631b1778c81
Anders Hedstrom <anders.hedstroem@gmail.com>
2019-02-12 17:45:29 +0100

    add fonts directory of configured

--- src/main/java/se/flinker/document/services/Html2PdfService.java
+++ src/main/java/se/flinker/document/services/Html2PdfService.java
@@ -1,29 +1,75 @@
 package se.flinker.document.services;
 
+import static java.util.Objects.nonNull;
+import static se.flinker.document.utils.LogUtil.warn;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Map;
 
+import javax.annotation.PostConstruct;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import com.itextpdf.html2pdf.ConverterProperties;
 import com.itextpdf.html2pdf.HtmlConverter;
+import com.itextpdf.html2pdf.resolver.font.DefaultFontProvider;
 import com.itextpdf.io.source.ByteArrayOutputStream;
+import com.itextpdf.layout.font.FontProvider;
 
 import se.flinker.document.exceptions.DocumentServiceAppRuntimeException;
 
 @Service
 public class Html2PdfService {
+    private static final Logger log = LoggerFactory.getLogger(Html2PdfService.class);
 
+    private String fontsDir;
+    private ConverterProperties properties;
+    
+    @Autowired
+    public Html2PdfService(@Value("${app.fontsDir}") String fontsDir) {
+        this.fontsDir = fontsDir;
+    }
+    
+    @PostConstruct
+    public void postConstruct() {
+        this.properties = createProperties(getClass().getSimpleName());
+    }
+    
     public byte[] generatePdf(Map<String, String> payload, String tx) {
         try {
             String html = payload.get("html");
             ByteArrayOutputStream baos = new ByteArrayOutputStream();
-            ConverterProperties props = new ConverterProperties();
-            props.setCharset("utf-8");
-            HtmlConverter.convertToPdf(html, baos, props);
+            HtmlConverter.convertToPdf(html, baos, properties);
             return baos.toByteArray();
         } catch (Exception e) {
             throw new DocumentServiceAppRuntimeException("problems converting html to pdf", tx, e);
         }
+    }
+    
+    private ConverterProperties createProperties(String tx) {
+        ConverterProperties props = new ConverterProperties();
+        props.setCharset("utf-8");
+
+        if (nonNull(fontsDir) && fontsDir.trim().length() > 0) {
+            try {
+                FontProvider fontProvider = new DefaultFontProvider();
+                Path path = Paths.get(fontsDir);
+                if (Files.exists(path) && Files.isDirectory(path) && Files.list(path).count() > 0) {
+                    fontProvider.addDirectory(fontsDir);
+                    props.setFontProvider(fontProvider);
+                }
+            } catch (IOException e) {
+                warn(tx, "Problems adding fonts directory: " + e.getMessage(), log);
+            }
+        }
+        return props;
     }
 
 }
